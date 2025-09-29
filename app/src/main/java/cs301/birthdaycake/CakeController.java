9deb295d58fd39069a830f45e006bb5876d19bf5
Felipe <lucaspab28@up.edu>
2025-09-29 15:32:28 -0700

    made the flames back to normal

--- app/src/main/java/cs301/birthdaycake/CakeController.java
+++ app/src/main/java/cs301/birthdaycake/CakeController.java
@@ -1,33 +1,14 @@
 package cs301.birthdaycake;
 
-import android.view.MotionEvent;
-import android.view.View;
-
-public class CakeController implements View.OnTouchListener {
+public class CakeController {
 
     private CakeView cakeView;
 
     private CakeModel modelOfCake;
 
-
     public CakeController (CakeView viewOfCake){
 
         this.cakeView = viewOfCake;
         this.modelOfCake = viewOfCake.getCakeModel();
-
     }
-
-    @Override
-    public boolean onTouch(View view, MotionEvent motionEvent) {
-        if (motionEvent.getAction() == MotionEvent.ACTION_DOWN) {
-            modelOfCake.x = motionEvent.getX();
-            modelOfCake.y = motionEvent.getY();
-            cakeView.invalidate();
-
-            return true;
-        }
-        return false;
-    }
-
-
 }
